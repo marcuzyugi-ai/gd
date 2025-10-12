f0a56782e591f29a4315014d2418744c40f0ff1a
marcuzyugi-ai <marcuzyugi@gmail.com>
2025-10-11 22:19:23 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,47 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.setGameOverMessage(false, "GAME OVER!")
+    if (game.ask("Retry")) {
+        info.changeLifeBy(-1)
+    }
+})
+controller.combos.attachCombo(controller.combos.idToString(controller.combos.ID.B), function () {
+    info.setLife(3)
+    light.showAnimation(light.rainbowAnimation, 500)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setVelocity(0, -100)
+    if (true) {
+        timer.after(900, function () {
+            mySprite.setVelocity(0, 100)
+        })
+    }
+})
+info.onLifeZero(function () {
+    game.reset()
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 5 5 5 5 5 5 5 5 5 5 5 5 5 5 8 
+    8 5 5 5 5 5 5 5 5 5 5 5 5 5 5 8 
+    8 5 8 8 8 8 5 5 5 8 8 8 8 5 5 8 
+    8 5 8 7 7 8 5 5 5 8 7 7 8 5 5 8 
+    8 5 8 7 7 8 5 5 5 8 7 7 8 5 5 8 
+    8 5 8 8 8 8 5 5 5 8 8 8 8 5 5 8 
+    8 5 5 5 5 5 5 5 5 5 5 5 5 5 5 8 
+    8 5 5 5 5 5 5 5 5 5 5 5 5 5 5 8 
+    8 5 8 8 8 8 8 8 8 8 8 8 8 8 5 8 
+    8 5 8 7 7 7 7 7 7 7 7 7 7 8 5 8 
+    8 5 8 7 7 7 7 7 7 7 7 7 7 8 5 8 
+    8 5 8 8 8 8 8 8 8 8 8 8 8 8 5 8 
+    8 5 5 5 5 5 5 5 5 5 5 5 5 5 5 8 
+    8 5 5 5 5 5 5 5 5 5 5 5 5 5 5 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+tiles.setCurrentTilemap(tilemap`level1`)
+mySprite.left = 0.25
+forever(function () {
+    mySprite.vx += 0.25
+    mySprite.vy += 5
+})
